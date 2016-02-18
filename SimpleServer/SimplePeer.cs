d78e46b6e52cc26b2cc4937a6b62f2c5a3a0e9ac
tien tran <tienrocker@gmail.com>
2016-02-18 17:56:12 +0700

    add source code

---
+++ SimpleServer/SimplePeer.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MyPeer.cs" company="Exit Games GmbH">
+//   Copyright (c) Exit Games GmbH.  All rights reserved.
+// </copyright>
+// <summary>
+//   Defines the MyPeer type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SimpleServer
+{
+    using ExitGames.Logging;
+
+    using Photon.SocketServer;
+
+    using PhotonHostRuntimeInterfaces;
+
+    public class SimplePeer : PeerBase
+    {
+        private readonly ILogger Log;
+        private IPhotonPeer photonPeer;
+        private IRpcProtocol protocol;
+
+        public SimplePeer(IRpcProtocol protocol, IPhotonPeer photonPeer, ILogger log) : base(protocol, photonPeer)
+        {
+            this.protocol = protocol;
+            this.photonPeer = photonPeer;
+            Log = log;
+        }
+
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+            if (Log.IsDebugEnabled)
+            {
+                Log.DebugFormat("OnDisconnect: conId={0}, reason={1}, reasonDetail={2}", this.ConnectionId, reasonCode, reasonDetail);
+            }
+        }
+
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+            if (Log.IsDebugEnabled)
+            {
+                Log.DebugFormat("OnOperationRequest: conId={0}, operationCode={1}, parameters={2}", this.ConnectionId, operationRequest.OperationCode, operationRequest.Parameters);
+            }
+        }
+    }
+}
---
+++ SimpleServer/SimpleServer.cs
@@ -0,0 +1,57 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SimpleServer.cs" company="Exit Games GmbH">
+//   Copyright (c) Exit Games GmbH.  All rights reserved.
+// </copyright>
+// <summary>
+//   Example application to show how to extend the Lite application.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SimpleServer
+{
+    using System.IO;
+    using ExitGames.Logging;
+    using log4net.Config;
+    using Photon.SocketServer;
+    using ExitGames.Logging.Log4Net;
+    /// <summary>
+    ///   Example application to show how to extend the Lite application.
+    /// </summary>
+    public class SimpleServer : ApplicationBase
+    {
+        private static readonly ILogger Log = LogManager.GetCurrentClassLogger();
+
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            return new SimplePeer(initRequest.Protocol, initRequest.PhotonPeer, Log);
+        }
+
+        protected override void Setup()
+        {
+
+            log4net.GlobalContext.Properties["Photon:ApplicationLogPath"] = Path.Combine(this.ApplicationRootPath, "log");
+
+            // log4net
+            string path = Path.Combine(this.BinaryPath, "log4net.config");
+            var file = new FileInfo(path);
+            if (file.Exists)
+            {
+                ExitGames.Logging.LogManager.SetLoggerFactory(Log4NetLoggerFactory.Instance);
+                XmlConfigurator.ConfigureAndWatch(file);
+            }
+
+            Log.InfoFormat("Created application Instance: type={0}", Instance.GetType());
+            Initialize();
+        }
+
+        protected void Initialize()
+        {
+            Protocol.AllowRawCustomValues = true;
+        }
+
+        protected override void TearDown()
+        {
+
+        }
+    }
+}
